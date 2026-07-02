c1ff932e226bbaaf7d18be30c2aa3b44580804d8
Mehdibkb <mehdi-yacine.boukabou@supdevinci-edu.fr>
2026-07-02 14:49:26 +0200

    correction probléme type de contrat

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ def fetch_jobs_via_ft(token, seen_links: set) -> list[dict]:
     # Paramètres de recherche (motsCles et typeContrat sont corrects)
     params = {
         "motsCles": "DevOps",
-        "typeContrat": "E2"
+        "typeContrat": "natureContrat"
     }
     
     response = requests.get(url, headers=headers, params=params)
